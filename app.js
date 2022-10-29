19455f4919fcda9ead4751afdfb69fabeb407bdf
lebo2003012000 <114799682+nishanth3012000@users.noreply.github.com>
2022-10-29 11:32:08 +0530

    input ouput done

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-var btntranslate = document.querySelector("#btn-Translate")
+var btntranslate = document.querySelector("#btn-translate");
+var txtinput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+
+function clickEventHandler() {
+    outputDiv.innerText = " nionmi " + txtinput.value;
+};
+
+btntranslate.addEventListener("click", clickEventHandler)
