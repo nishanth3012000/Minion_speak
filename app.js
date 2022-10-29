64d9727452922f5ca5f5cc713c29decfb3911725
lebo2003012000 <114799682+nishanth3012000@users.noreply.github.com>
2022-10-29 12:32:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ var btntranslate = document.querySelector("#btn-translate");
 var txtinput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
 
 function clickEventHandler() {
-    outputDiv.innerText = " nionmi " + txtinput.value;
+
+
+
+    var inputText = txtinput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => console.log(json.contents.translated))
 };
 
 btntranslate.addEventListener("click", clickEventHandler)
